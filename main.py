f2a6275ed2dccd9eb4bda75efff555bfd0878d75
ehfrosee <ehfros.e.e@gmail.com>
2024-12-11 23:52:27 +0300

    Усрешный пробный запрос

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import openai
 from openai import OpenAI
 import os
 
-#"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"
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 print(openai.api_key)
 client = OpenAI()
